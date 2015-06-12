3a807fd48dc4af05ba3b9fc6fe33028edabaa6ac
neelabh gupta <neelabh.gupta@urbanladder.com>
2015-06-12 12:29:38 +0530

    added test case for procurement dept

--- spec/organization/procurement_department_spec.rb
+++ spec/organization/procurement_department_spec.rb
@@ -16,19 +16,29 @@ describe Organization::ProcurementDepartment do
   end
 
   context 'category stats' do
-    it "should return inventory as 200 for a Procurement Department with 200 black products" do
+    it "should return 200 inventory for black for a Procurement Department with 200 black products" do
       department1 = FactoryGirl.build(:procurement_department, inventory: 200, category_attributes: {"color" => "Black"})
       expect(department1.inventory_by_category('color', 'Black')).to eq(200)
     end
 
-    it "should return inventory as 0 for a Procurement Department with 200 green products" do
+    it "should return 0 inventory for black for a Procurement Department with 200 green products" do
       department1 = FactoryGirl.build(:procurement_department, inventory: 200, category_attributes: {"color" => "Green"})
       expect(department1.inventory_by_category('color', 'Black')).to eq(0)
     end
 
-    it "should return inventory as 200 for a Procurement Department with 200 green products" do
+    it "should return 200 inventory for green for a Procurement Department with 200 green products" do
       department1 = FactoryGirl.build(:procurement_department, inventory: 200, category_attributes: {"color" =>  "Green"})
       expect(department1.inventory_by_category('color', 'Green')).to eq(200)
+    end
+
+    it "should return 0 inventory for black clothes which have funding less that 100" do
+      department = FactoryGirl.build(:procurement_department, inventory: 300, category_attributes: {"color" => "Yellow"}, cash: 230)
+      expect(department.inventory_for_underfunded_colors('Black', 100)).to eq(0)
+    end
+
+    it "should return 20 inventory for Orange clothes which have funding less that 90" do
+      department = FactoryGirl.build(:procurement_department, inventory: 20, category_attributes: {"color" => "Orange"}, cash: 30)
+      expect(department.inventory_for_underfunded_colors('Orange', 90)).to eq(20)
     end
   end
 
